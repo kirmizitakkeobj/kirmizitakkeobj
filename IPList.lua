a12946751ed107d83ecbfa624cc564d804b99532
kirmizitakkeobj <82776818+kirmizitakkeobj@users.noreply.github.com>
2021-04-18 21:53:04 +0300

    Create IPList.lua

---
+++ IPList.lua
@@ -0,0 +1,25 @@
+IPlist = {
+  "213.226.119.27", --ALCHERA ROLEPLAY  
+  "89.252.161.144"  --Cayp LOCAL
+}
+oneat = nil
+function Magic(res)
+    name = getServerConfigSetting("servername")
+    fetchRemote("https://api.my-ip.io/ip", function(ip)
+        print("IP Kontrol ediliyor....")
+        serverip = ip 
+        if table.find(IPlist, serverip) then
+        	if oneat then return end
+            setElementData(root, "setElementData", 1)
+            print ("Cayp Model orjinal obje kullandiginiz icin tesekkurler.")
+            sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..name.. " **Obje Aktif!**" )
+            oneat = true
+        else 
+            shutdown ("Cayp Model calinti obje tespit edildi iyi ucuslar!")
+            print("Cayp Model calinti obje tespit edildi iyi ucuslar")
+            setElementData(root, "setElementData", nil)
+            sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Yok!  ** Server Name : " ..name.. " **Server Kapatıldı!**" )
+        end
+    end)    
+end
+addEventHandler ( "onResourceStart", getRootElement(), Magic )
