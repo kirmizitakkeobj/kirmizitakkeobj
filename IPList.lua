d42cb134bf1730bd60f20801d126689b8cae37ad
kirmizitakkeobj <82776818+kirmizitakkeobj@users.noreply.github.com>
2021-04-19 08:45:36 +0300

    Update IPList.lua

--- IPList.lua
+++ IPList.lua
@@ -1,11 +1,12 @@
 IPlist = {
   "213.226.119.27", --ALCHERA ROLEPLAY  
-  "89.252.161.1444444444444444"  --Cayp LOCAL
+  "89.252.161.144"  --Cayp LOCAL
 }
 oneat = nil
 function Magic(res)
     name = getServerConfigSetting("servername")
     fetchRemote("https://api.my-ip.io/ip", function(ip)
+     print("IP Kontrol ediliyor....")
         serverip = ip 
         if table.find(IPlist, serverip) then
         	if oneat then return end
