7171ae3608c2d6ba7ea4491528047440c6228c96
kirmizitakkeobj <82776818+kirmizitakkeobj@users.noreply.github.com>
2021-04-19 18:20:02 +0300

    Update IPList.lua

--- IPList.lua
+++ IPList.lua
@@ -19,7 +19,7 @@ function Magic(res)
         	if oneat then return end
             setElementData(root, "setElementData", 1)
             print ("Cayp Model orjinal obje kullandiginiz icin tesekkurler.")
-            sendMessage("objelog", "**Obje Adı: **" ..getResourceName(res) .."\n**Sunucu IP: **"..ip.." **\nLisans:** Var""**\nSunucu Adı: **"..name)
+            sendMessage("objelog", getResourceName(res).. " adlı objeyi kullanan server ip :   "  ..ip.." Lisans:  **Var!  ** Server Name : " ..name.. " **Obje Aktif!**" )
             oneat = true
         else 
             shutdown ("Cayp Model calinti obje tespit edildi iyi ucuslar!")
